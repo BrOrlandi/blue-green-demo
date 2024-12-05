d6b2f777522f09f52931504f82ae6f7c0d6ce21e
BrOrlandi <brorlandi@gmail.com>
2024-12-05 03:13:08 -0300

    Working blue green deploy.

--- app-sample/index.js
+++ app-sample/index.js
@@ -1,27 +1,36 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 
 // Atraso de 5 segundos antes de o servidor começar a ouvir
 setTimeout(() => {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log(`Servidor ouvindo na porta ${PORT}`);
+    console.log(`Servidor ouvindo http://localhost:${PORT}`);
   });
 }, 5000);
 
 // Rota para responder com 'Hello World' e as variáveis de ambiente
 app.get('/', (req, res) => {
-  res.send(
-    `<h1 style="
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-">Hello World!<br/>
+  const filePath = path.join(__dirname, 'index.html');
 
-NODE_ENV: ${process.env.NODE_ENV}<br/>
-ENVIRONMENT: ${process.env.ENVIRONMENT}
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Erro ao carregar a página');
+    }
 
-</h1>`
-  );
+    // Substitui as variáveis no HTML
+    const result = data.replace(
+      /\$\{DEPLOYMENT\}/g,
+      process.env.DEPLOYMENT || 'unknown'
+    );
+
+    res.send(result);
+  });
+});
+
+app.get('/healthcheck', (req, res) => {
+  res.send('ok!');
 });
