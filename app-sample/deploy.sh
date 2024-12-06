5573b64010e0397c87f582caa3314dc05926e9a0
BrOrlandi <brorlandi@gmail.com>
2024-12-06 01:23:41 -0300

    BG working.

--- app-sample/deploy.sh
+++ app-sample/deploy.sh
@@ -21,7 +21,7 @@ echo "Starting "$CURRENT_DEPLOYMENT" container"
 
 DEPLOYMENT=$DEPLOYMENT docker compose --project-name=$CURRENT_DEPLOYMENT up -d --build
 
-echo 'Waiting Strapi API to succeed in its healthcheck'
+echo 'Waiting API to succeed in its healthcheck'
 until docker compose --project-name=$CURRENT_DEPLOYMENT exec -T ${PROJECT_NAME_PREFIX} curl --silent --head --fail http://0.0.0.0:3000/healthcheck ; do
     echo '.'
     sleep 3
--- app-sample/index.js
+++ app-sample/index.js
@@ -4,18 +4,8 @@ const path = require('path');
 
 const app = express();
 
-// Atraso de 5 segundos antes de o servidor começar a ouvir
-setTimeout(() => {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Servidor ouvindo http://localhost:${PORT}`);
-  });
-}, 5000);
-
-// Rota para responder com 'Hello World' e as variáveis de ambiente
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'index.html');
-
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       return res.status(500).send('Erro ao carregar a página');
@@ -34,3 +24,11 @@ app.get('/', (req, res) => {
 app.get('/healthcheck', (req, res) => {
   res.send('ok!');
 });
+
+// Atraso de 5 segundos antes de o servidor começar a ouvir
+setTimeout(() => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor ouvindo http://localhost:${PORT}`);
+  });
+}, 5000);
